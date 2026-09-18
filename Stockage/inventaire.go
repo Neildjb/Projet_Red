f984ff6108f3fff4c1cdd4555ca276e5e961e756
Neil <neil.djebali@ynov.com>
2026-09-18 16:34:17 +0200

    modif 1

--- Stockage/inventaire.go
+++
@@ -1,8 +0,0 @@
-package inventaire
-
-import "Projet_Red/personnage"
-import "fmt"
-
-func AccessInventory(e personnage.Etudiant) {
-	fmt.Print(e.Inventaire)
-}
--- inventaire/inventaire.go
+++ inventaire/inventaire.go
@@ -1,1 +1,8 @@
-test
+package inventaire
+
+import "Projet_Red/personnage"
+import "fmt"
+
+func AccessInventory(e personnage.Etudiant) {
+	fmt.Print(e.Inventaire)
+}
