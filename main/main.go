6c6cb077619d4038f64f0c1f46db559e9a2bc14d
andyabbaswilfried-cell <andyabbaswilfried@gmail.com>
2026-09-18 16:15:05 +0200

    Personnage étudiant

--- main/main.go
+++ main/main.go
@@ -1,11 +1,14 @@
 package main
 
+
 import (
 	"Projet_Red/personnage"
 	"fmt"
+	"Projet_Red/tache8"
 )
 
 func main() {
 	c1 := personnage.InitCharacter("Kael", "Etudiant", 1, 10, 5, []string{})
+	tache8.isdead()
 	fmt.Print(c1)
 }
