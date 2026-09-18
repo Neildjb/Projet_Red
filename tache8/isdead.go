44973daaa5f68de447ecfb4fecd086dbca074899
andyabbaswilfried-cell <andyabbaswilfried@gmail.com>
2026-09-18 16:03:45 +0200

    Personnage étudiant

---
+++ tache8/isdead.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"Projet_Red/personnage"
+	"fmt"
+)
+
+func isdead(e personnage.Etudiant) {
+	if e.Vie == 0 {
+		fmt.Println("tu es mort mais tout le monde a le droit à une seconde chance")
+		e.Vie = 50
+		fmt.Println("tu es  revivant")
+	}
+}
+
+
