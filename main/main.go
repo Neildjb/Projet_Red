911d1f4cec825fc54bb6e885be558000f2cac424
andyabbaswilfried-cell <andyabbaswilfried@gmail.com>
2026-09-21 10:53:43 +0200

    Miseenplacedumainv1

--- main/main.go
+++ main/main.go
@@ -5,10 +5,30 @@ import (
 	"Projet_Red/tache8"
 	"fmt"
 	"strconv"
+	"Projet_Red/tache7101111suite"
 )
 
 func main() {
-	c1 := personnage.InitCharacter("Kael", "Etudiant", 1, 10, 5, []string{}, personnage.Equipment{})
+	fmt.Println("ecrit moi ton nom")	
+	
+	var choix string
+	var choix2 string
+	
+
+	_, err := fmt.Scanln(&choix)
+
+	if err != nil {
+		fmt.Println("Erreur de lecture sur le blaze")
+	}
+	fmt.Println("ecrit moi ton class")	
+	_, err2 := fmt.Scanln(&choix2)
+
+	if err2 != nil {
+		fmt.Println("Erreur de lecture sur le deuxième choix")
+	}
+
+	c1 := tache7101111suite.CharacterCreation(choix, choix2)
+	personnage.DisplayInfo(c1)
 	tache8.Isdead(c1)
 	fmt.Print("t'as que " + strconv.Itoa(c1.Argent) + " euros pour commencer sale pauvre ")
 	fmt.Print(c1.Equipement)
