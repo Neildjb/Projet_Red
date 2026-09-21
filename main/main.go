2213fb50e2208040c63e0ff5fc23e79841966a90
FONTAGNE Mathias <mathias.fontagne@ynov.com>
2026-09-21 16:51:36 +0200

    combatv2

--- main/main.go
+++ main/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
+	"Projet_Red/Menu"
 	"Projet_Red/combat"
+	"Projet_Red/inventaire"
 	"Projet_Red/personnage"
 	"Projet_Red/tache7101111suite"
-	"Projet_Red/Menu"
-	"Projet_Red/inventaire"
 	"fmt"
 	"strconv"
-	"Projet_Red/tache8"
 )
 
 func main() {
 	fmt.Println("Bienvenue dans le jeu _")
 	fmt.Println("Vous êtes un Ninja et vous devez combattre des monstres pour devenir le plus fort.")
 	fmt.Println("Commencons par la creation de votre personnage.")
-	fmt.Println("Ecrit moi ton nom :")	
-	
+	fmt.Println("Ecrit moi ton nom :")
+
 	var choix string
 	var choix2 string
 
@@ -26,7 +25,7 @@ func main() {
 	}
 
 	fmt.Println("La classe que tu choisi difini le mode de difficulte du Jeu, Kage = facile, Jonin = moyen, Genin = difficile, ninja = impossible")
-	fmt.Println("ecrit moi ton class(difficulté du jeu) :")	
+	fmt.Println("ecrit moi ton class(difficulté du jeu) :")
 
 	_, err2 := fmt.Scanln(&choix2)
 
@@ -42,13 +41,13 @@ func main() {
 	fmt.Print("Commencons par vous expliquer coment acceder au menu")
 	fmt.Println("il faut ecrire 'menu()' et appuyer sur entrer")
 	Menu.Menu(c1)
-	tache8.Isdead(c1)
 	fmt.Print("t'as que " + strconv.Itoa(c1.Argent) + " euros pour commencer sale pauvre ")
 	fmt.Print(c1.Equipement)
 	c1.Vie = 0
-	tache8.Isdead(c1)
-	tache7101111suite.AddInventory(&c1,"Kunaï")
+	tache7101111suite.AddInventory(&c1, "Kunaï")
 	inventaire.AccessInventory(c1)
-	tache7101111suite.RemoveInventory(&c1,"Kunaï")
+	tache7101111suite.RemoveInventory(&c1, "Kunaï")
 	inventaire.AccessInventory(c1)
+	monstre := personnage.Init_Golems_de_chakra()
+	combat.Combat(&monstre, &c1)
 }
